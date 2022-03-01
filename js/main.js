d71de05ad81d9ffbaace8d703d3051b8f1a18e8f
mdkawsarali <facebookerjordan@gmail.com>
2022-03-01 18:24:37 +0600

    Everything Done!

--- js/main.js
+++ js/main.js
@@ -35,12 +35,12 @@ const displayResult = data => {
 
         div.innerHTML = `
             <div class="card">
-                <figure onclick="loadDetails('${slug}')">
+                <figure onclick="loadDetails('${slug}')" data-bs-toggle="modal" data-bs-target="#phoneDetails">
                     <img src="${image}" class="card-img-top" alt="${phone_name}" />
                 </figure>
                 <div class="card-body">
                     <div class="d-flex align-items-center justify-content-between">
-                        <h5 onclick="loadDetails('${slug}')" class="card-title mb-0">${phone_name}</h5>
+                        <h5 onclick="loadDetails('${slug}')" class="card-title mb-0" data-bs-toggle="modal" data-bs-target="#phoneDetails">${phone_name}</h5>
                         <h6 class="mb-0">Brand: ${brand}</h6>
                     </div>
                 </div>
@@ -55,11 +55,66 @@ const displayResult = data => {
     });
 
     // Display Show All Button
-    if (data.length >= 12) {
+    if (data.length > 12) {
         showAllBtn.classList.remove('d-none');
     } else {
         showAllBtn.classList.add('d-none');
     }
+}
+
+// Load Phone Details (Function)
+const modalPreloader = document.querySelector('.preloader');
+const singleName = document.getElementById('name');
+const singleImage = document.getElementById('image');
+const singleBrand = document.getElementById('brand');
+const singleReleased = document.getElementById('released');
+const singleStorage = document.getElementById('storage');
+const singleDisplay = document.getElementById('display');
+const singleChipSet = document.getElementById('chipset');
+const singleMemory = document.getElementById('memory');
+const singleSensors = document.getElementById('sensors');
+const singleWlan = document.getElementById('wlan');
+const singleUsb = document.getElementById('usb');
+
+const loadDetails = async slug => {
+    // Show Preloader
+    modalPreloader.style.display = 'flex';
+    modalPreloader.style.opacity = '1';
+
+    // Fetch API
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    const response = await fetch(url);
+    const results = await response.json();
+    const data = results.data;
+
+    // Object Destructuring (With default value for undefined Property)
+    const {
+        name, image, brand, releaseDate,
+        mainFeatures: { storage, displaySize, chipSet, memory, sensors } = { storage: 'N/A', displaySize: 'N/A', chipSet: 'N/A', memory: 'N/A', sensors: 'N/A' },
+        others: { WLAN, USB } = { WLAN: 'N/A', USB: 'N/A' }
+    } = data;
+
+    // Get All Items from Sensors
+    const allSensors = sensors?.map(sensor => ' ' + sensor);
+
+    // Display All Information
+    if (data) {
+        singleName.innerText = name;
+        singleImage.src = image;
+        singleBrand.innerText = brand;
+        singleReleased.innerText = releaseDate;
+        singleStorage.innerText = storage;
+        singleDisplay.innerText = displaySize;
+        singleChipSet.innerText = chipSet;
+        singleMemory.innerText = memory;
+        singleSensors.innerText = allSensors;
+        singleWlan.innerText = WLAN;
+        singleUsb.innerText = USB;
+    }
+
+    // Hide Preloader
+    modalPreloader.style.opacity = '0';
+    modalPreloader.style.display = 'none';
 }
 
 // Search Phone
